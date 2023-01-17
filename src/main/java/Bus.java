c5c7c72d84517e880dad4a912d80a451d2862d27
Benjamin Smith <bsmith@cabbage.org.uk>
2023-01-17 16:14:15 +0000

    Tests for Bus adding Passengers

--- src/main/java/Bus.java
+++ src/main/java/Bus.java
@@ -1,2 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Bus {
+    private final String destination;
+    private final int capacity;
+    private final List<Person> passengers;
+
+    public Bus(String destination, int capacity) {
+        this.destination = destination;
+        this.capacity = capacity;
+        this.passengers = new ArrayList<>();
+    }
+
+    public int numberOfPassengers() {
+        return passengers.size();
+    }
+
+    public boolean addPassenger(Person p) {
+        if (this.numberOfPassengers() < this.capacity)
+            return passengers.add(p);
+        return false;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public Person removePassenger() {
+        /* TODO: unimplemented */
+        return null;
+    }
 }
--- src/test/java/BusTest.java
+++ src/test/java/BusTest.java
@@ -4,4 +4,45 @@ import org.junit.Before;
 import static org.junit.Assert.*;
 
 public class BusTest {
+    private Bus bus;
+
+    @Before
+    public void setUp() {
+        bus = new Bus("Mars", 3);
+    }
+
+    @Test
+    public void startsEmpty() {
+        assertEquals(0, bus.numberOfPassengers());
+    }
+
+    @Test
+    public void hasDestination() {
+        assertEquals("Mars", bus.getDestination());
+    }
+
+    @Test
+    public void canAddOnePassenger() {
+        assertTrue(bus.addPassenger(new Person()));
+        assertEquals(1, bus.numberOfPassengers());
+    }
+
+    @Test
+    public void canOnlyAddThreePassengers() {
+        /* capacity was set to 3 */
+        for (int i = 0; i < 3; i++)
+            assertTrue(bus.addPassenger(new Person()));
+        /* 4th passenger will fail */
+        assertFalse(bus.addPassenger(new Person()));
+        assertEquals(3, bus.numberOfPassengers());
+    }
+
+    /* TODO: removing passengers not yet implemented */
+    @Test(expected = AssertionError.class)
+    public void canRemovePassenger() {
+        bus.addPassenger(new Person());
+        Object passenger = bus.removePassenger();
+        assertNotNull(passenger);
+        assertEquals(0, bus.numberOfPassengers());
+    }
 }
